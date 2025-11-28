73d098a217768d60852e4b0a920d545cd2c51bfd
nopi-max <novirahayu823@gmail.com>
2025-11-28 09:21:37 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+// Script sederhana untuk interaksi tombol
+document.getElementById("btnClick").addEventListener("click", function () {
+    document.getElementById("output").textContent = "Terima kasih! Tombol berhasil diklik 🎉";
+});
